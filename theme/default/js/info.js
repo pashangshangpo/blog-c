9aabbbc7746f99d35d019ceeb7f9f1c402af92e1
爬上上坡 <pashangshangpo@gmail.com>
2018-03-10 20:55:06 +0800

    更换评论系统

--- theme/default/js/info.js
+++
@@ -1,9 +0,0 @@
-/**
- * 详情页
-*/
-
-// 评论功能
-const script = document.createElement('script');
-script.src = 'https://pssp.disqus.com/embed.js';
-script.setAttribute('data-timestamp', +new Date());
-document.body.appendChild(script);
